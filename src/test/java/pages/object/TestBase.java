71f036e1328078b064c2d6acb586559509feb70d
Dark <darkuiozzz@gmail.com>
2025-02-24 23:22:36 +0300

    Homework15 part 2/2 new remote addres

--- src/test/java/pages/object/TestBase.java
+++ src/test/java/pages/object/TestBase.java
@@ -1,16 +1,12 @@
 package pages.object;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.object.enums.Browser;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import static com.codeborne.selenide.Browsers.CHROME;
 import static com.codeborne.selenide.Browsers.FIREFOX;
@@ -21,6 +17,7 @@ import static pages.object.helpers.Locators.getLocator;
 public class TestBase {
     Browser browsers = Browser.valueOf(System.getProperty("browser", "chrome"));
     DesiredCapabilities caps = new DesiredCapabilities();
+
     @BeforeMethod
     public void methodSetup() {
         switch (browsers) {
@@ -35,16 +32,16 @@ public class TestBase {
                 caps.setPlatform(Platform.LINUX);
                 break;
         }
-            Configuration.timeout = 5000;
-            Configuration.pageLoadTimeout = 5000;
-            Configuration.browserSize = "1920x1080";
-            Configuration.remote = "http://192.168.0.104:4444/wd/hub";
-            Configuration.browserCapabilities = caps;
-            open(getLocator("Test.url"));
+        Configuration.timeout = 5000;
+        Configuration.pageLoadTimeout = 5000;
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "http://169.254.172.221:4444/wd/hub";
+        Configuration.browserCapabilities = caps;
+        open(getLocator("Test.url"));
 
     }
 
-        @AfterMethod
+    @AfterMethod
     public void methodTeardown() {
         Selenide.closeWebDriver();
     }
